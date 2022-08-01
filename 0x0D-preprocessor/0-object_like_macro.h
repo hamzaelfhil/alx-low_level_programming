7ffab553807b2cb5fdf052200299fe37be0bb61b
hamzaelfhil <hamzaelfhil@icloud.com>
2022-08-01 12:19:42 -0700

    preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif /* end _OBJECT_LIKE_MACRO_H */
+//////////////   1
+#ifndef _PI_H
+#define _PI_H
+
+#define PI 3.14159265359
+
+#endif /* END _PI_H */
