4eb2cf74367e12cf32b366bd776881f68636dbcd
hamzaelfhil <hamzaelfhil@icloud.com>
2023-05-31 18:58:47 -0700

    all task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,17 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+char *string_toupper(char *s);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
-#endif
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+#endif /* end main.h */
