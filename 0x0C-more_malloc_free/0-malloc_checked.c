c6c47fac73a2186646d05f9298fe50b831e7cbb7
hamzaelfhil <hamzaelfhil@icloud.com>
2023-06-15 19:10:23 -0700

    all tasks

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: int.
+ * Return: a pointer to allocate memeory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
